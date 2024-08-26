d0de1fa446fd07ca79955ff3bee5563d03b3a732
Viktor <limphanith.dev@gmail.com>
2024-08-26 15:08:46 +0700

    finish notification-parser

--- notification-channel-parser/Program.cs
+++ notification-channel-parser/Program.cs
@@ -1,4 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
 
+namespace notification_channel_parser
+{
+    internal abstract class Program
+    {
+        private static void Main()
+        {
+            const string input = "[HAHA][BE][QA] there is error";
+            
+            // Define pattern and validChannel
+            List<string> validChannels = ["BE", "FE", "Urgent", "QA"];
+            const string pattern = @"\[(.*?)\]";
+            
+            // Get the value that valid the pattern and channels
+            List<string> foundChannels = GetMatches(input, pattern, validChannels);
 
-Console.WriteLine("Hello, World!");
+            string result = "Receive channels: " + string.Join(", ", foundChannels);
+            Console.WriteLine(result);
+        }
+        
+        private static List<string> GetMatches(string input, string pattern, List<string> validChannel)
+        {
+            var matchesList = new List<string>();
+            var matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                var value = match.Groups[1].Value;
+                if (validChannel.Contains(value) && !matchesList.Contains(value))
+                {
+                    matchesList.Add(value);
+                }
+            }
+            return matchesList;
+        }
+    }
+}
+
